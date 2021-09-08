8ebf374522242a35f48bf49045dbcd89685c6d48
filipef9 <filipef9@hotmail.com>
2021-09-08 03:18:20 +0000

    Instalação do primeflex.

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 import PrimeVue from 'primevue/config';
+
 import 'primevue/resources/themes/saga-blue/theme.css';
 import 'primevue/resources/primevue.min.css';
 import 'primeicons/primeicons.css';  
+import 'primeflex/primeflex.min.css';
+
 import Button from 'primevue/button';
 import InputText from 'primevue/inputtext';
 
