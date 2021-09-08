5a179e92c595be126c8181872d3a1c9890e2190e
filipef9 <filipef9@hotmail.com>
2021-09-08 03:19:23 +0000

    Atualização do layout.

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,37 @@
 <template>
-  <header>
+  <header class="bg-primary py-1 pl-4">
     <h2>Reais para Dólares</h2>
   </header>
-  <main>
-    <br /><br />
-    <span class="p-float-label">
+
+  <main class="flex flex-column align-items-center">
+    <div class="mt-5">
+      <i class="pi pi-dollar text-6xl font-bold"></i>
+    </div>
+
+    <div class="p-float-label mt-5">
       <InputText
         id="quantidadeEmReais"
         type="text"
         v-model="quantidadeEmReais"
       />
       <label for="quantidadeEmReais">Quantidade em Reais</label>
-    </span>
-
-    <br /><br />
-    <span class="p-float-label">
-      <InputText id="cotacaoEmDolar" type="text" v-model="cotacaoDoDolar" />
-      <label for="cotacaoEmDolar">Cotação do Dólar</label>
-    </span>
-
-    <br /><br />
-    <div>
-      <Button label="Limpar" @click="limpar" />
-      <Button label="Calcular" @click="calcular" />
     </div>
 
-    <br /><br />
-    <div v-show="resultadoConversao">
-      <p>Com {{ quantidadeEmReais }} reais é possível comprar {{ resultadoConversao }} dólares a {{ cotacaoDoDolar }} cada</p>
+    <div class="p-float-label mt-5">
+      <InputText id="cotacaoEmDolar" type="text" v-model="cotacaoDoDolar" />
+      <label for="cotacaoEmDolar">Cotação do Dólar</label>
+    </div>
+
+    <div class="mt-5">
+      <Button label="Limpar" @click="limpar" class="mr-1" />
+      <Button label="Calcular" @click="calcular" class="ml-1" />
+    </div>
+
+    <div v-show="resultadoConversao" class="mt-5">
+      <p>
+        Com <strong>{{ quantidadeEmReais }}</strong> reais é possível comprar
+        <strong>{{ resultadoConversao }}</strong> dólares a <strong>{{ cotacaoDoDolar }}</strong> cada
+      </p>
     </div>
   </main>
 </template>
@@ -48,9 +52,11 @@ export default {
       this.resultadoConversao = null;
     },
     calcular() {
-      this.resultadoConversao = (this.quantidadeEmReais / this.cotacaoDoDolar).toFixed(2);
-    }
-  }
+      this.resultadoConversao = (
+        this.quantidadeEmReais / this.cotacaoDoDolar
+      ).toFixed(2);
+    },
+  },
 };
 </script>
 
@@ -59,7 +65,5 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
