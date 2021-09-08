735c2b9f330bf114f6594552b150ae18034ba810
filipef9 <filipef9@hotmail.com>
2021-09-08 00:02:09 +0000

    Implementação do cálculo da conversão de BRL para USD.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,57 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <header>
+    <h2>Reais para Dólares</h2>
+  </header>
+  <main>
+    <br /><br />
+    <span class="p-float-label">
+      <InputText
+        id="quantidadeEmReais"
+        type="text"
+        v-model="quantidadeEmReais"
+      />
+      <label for="quantidadeEmReais">Quantidade em Reais</label>
+    </span>
+
+    <br /><br />
+    <span class="p-float-label">
+      <InputText id="cotacaoEmDolar" type="text" v-model="cotacaoDoDolar" />
+      <label for="cotacaoEmDolar">Cotação do Dólar</label>
+    </span>
+
+    <br /><br />
+    <div>
+      <Button label="Limpar" @click="limpar" />
+      <Button label="Calcular" @click="calcular" />
+    </div>
+
+    <br /><br />
+    <div v-show="resultadoConversao">
+      <p>Com {{ quantidadeEmReais }} reais é possível comprar {{ resultadoConversao }} dólares a {{ cotacaoDoDolar }} cada</p>
+    </div>
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      quantidadeEmReais: null,
+      cotacaoDoDolar: null,
+      resultadoConversao: null,
+    };
+  },
+  methods: {
+    limpar() {
+      this.quantidadeEmReais = null;
+      this.cotacaoDoDolar = null;
+      this.resultadoConversao = null;
+    },
+    calcular() {
+      this.resultadoConversao = (this.quantidadeEmReais / this.cotacaoDoDolar).toFixed(2);
+    }
   }
-}
+};
 </script>
 
 <style>
@@ -19,7 +59,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
